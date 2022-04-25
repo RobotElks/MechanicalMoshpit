3094552d4b5e9c388bea40c51bac212dd0c00141
Tom Karlsson <tomk4@kth.se>
2022-04-25 15:49:38 +0200

    update parse method

--- Assets/Scripts/WorldParse.cs
+++ Assets/Scripts/WorldParse.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using System.IO;
 using UnityEngine;
 
@@ -14,21 +15,36 @@ public class WorldParse : MonoBehaviour
 
     void Parse(byte[] input, int n)
     {
-        int length = input.Length;
-        int new_index;
-        switch(input[n]){
-            // 35 = '#'
-            case 35:
-                new_index = Comment(input, n);
-                break;
-            // 123 = '{'
-            case 123:
-                new_index = Load(input, n);
-                break;
-            default:
-                Debug.Log("Error, wrong format in world file!");
-                break;
+        //Debug.Log("In parse with n : " + n);
+        //Debug.Log("Array length : " + input.Length);
+
+        if(n < input.Length)
+        {
+            //Debug.Log("Char at newline : " + (char)input[n] + " : byte = " + input[n]);
+            int new_index = 0;
+
+            switch (input[n])
+            {
+                // 35 = '#'
+                case 35:
+                    new_index = Comment(input, n);
+                    break;
+                // 123 = '{'
+                case 123:
+                    new_index = Load(input, n);
+                    break;
+                default:
+                    Debug.Log("Error, wrong format in world file!");
+                    throw new Exception("Wrong format in level file");          
+            }
+
+            Parse(input, new_index);
         }
+        else
+        {
+            Debug.Log("Load done!");
+        }
+
     }
 
     int Comment(byte[] input, int n)
@@ -39,11 +55,11 @@ public class WorldParse : MonoBehaviour
 
     int Load(byte[] input, int n)
     {
-        return n;
+        while (n < input.Length && input[n] != '\n')
+        {
+            Debug.Log((char)input[n++]);
+        }
+        return ++n;
     }
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    
-    //}
+
 }
