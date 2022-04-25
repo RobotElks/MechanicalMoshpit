a21e8091fa1b397d0dff0069270d94b89e4e1abe
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2022-04-25 16:43:51 +0200

    All rotate and movement implemented

--- Assets/Scripts/RobotMovement.cs
+++ Assets/Scripts/RobotMovement.cs
@@ -11,28 +11,44 @@ public class RobotMovement : MonoBehaviour
 {
 
     Vector3 positionTarget;
+    Vector3 rotationTarget;
     public int tileSize;
-    public float movementSpeed;
-    public float gear;
-    bool startedMove = false;
-    public float turnRate = 90.0f;
+    float movementSpeed;
+    float movementGear;
+    float rotateGear;
+    public bool startedMove = false;
+    public bool startedRotation = false;
+    float turnRate = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.Rotate(0, 90, 0);
-        MoveBackwards();
-        //SetGear(Gear.First);
+        SetGear(Gear.First);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (IsMoving())
+
+        if (startedRotation)
+        {
+            if((transform.forward - rotationTarget).magnitude > 0.006f)
+            {
+                transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, rotationTarget, turnRate * rotateGear * Time.deltaTime, 0.0f));
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.RoundToInt(transform.eulerAngles.y), transform.eulerAngles.z);
+                startedRotation = false;  
+            }
+        }
+
+
+        else if (startedMove)
         {
             if ((transform.position - positionTarget).magnitude > 0.000001f)
             {
-                transform.position = Vector3.MoveTowards(transform.position, positionTarget, movementSpeed * gear * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, positionTarget, movementSpeed * movementGear * Time.deltaTime);
             }
             else
             {
@@ -63,17 +79,19 @@ public class RobotMovement : MonoBehaviour
 
     public void RotateLeft()
     {
-       
+        rotationTarget = -transform.right;
+        startedRotation = true;
     }
 
     public void RotateRight()
     {
-        
+        rotationTarget = transform.right;
+        startedRotation = true;
     }
 
     public bool IsRotating()
     {
-        return true;
+        return startedRotation;
     }
 
     public void SetGear(Gear newGear)
@@ -81,13 +99,16 @@ public class RobotMovement : MonoBehaviour
         switch (newGear)
         {
             case Gear.First:
-                gear = 1;
+                movementGear = 1;
+                rotateGear = 1;
                 break;
             case Gear.Second:
-                gear = 2;
+                movementGear = 2;
+                rotateGear = 1.5f;
                 break;
             case Gear.Third:
-                gear = 3;
+                movementGear = 3;
+                rotateGear = 2;
                 break;
         }
 
