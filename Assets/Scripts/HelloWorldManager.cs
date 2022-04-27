a6a352c7dbe37b3f39ff212a7afdda885ddefbab
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2022-04-27 15:40:17 +0200

    First working moving robots

--- Assets/Scripts/HelloWorldManager.cs
+++ Assets/Scripts/HelloWorldManager.cs
@@ -40,25 +40,28 @@ namespace HelloWorld
             GUILayout.Label("Mode: " + mode);
         }
 
-        static void SubmitNewPosition()
-        {
-            if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change"))
-            {
-                if (NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient)
-                {
-                    foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
-                        NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<Multiplayer>().Move();
-                }
-                else
-                {
-                    var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
-                    var player = playerObject.GetComponent<Multiplayer>();
-                    player.Move();
-                }
-            }
-        }
+        static void SubmitNewPosition() { }
+        //{
+        //    if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move Forward" : "Request Position Change"))
+        //    {
+        //        if (NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient)
+        //        {
+        //            foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
+        //            {
+        //                NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<RobotMultiplayerMovement>().Move();
+        //                Debug.Log(uid);
+        //            }
+        //        }
+        //        else
+        //        {
+        //            var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
+        //            var player = playerObject.GetComponent<RobotMultiplayerMovement>();
+        //            //player.Move();
+        //        }
+        //    }
 
-        
+
+
 
     }
 }
---
+++ Assets/Scripts/RobotMultiplayerMovement.cs
@@ -0,0 +1,166 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class RobotMultiplayerMovement : NetworkBehaviour
+{
+    public NetworkVariable<Vector3> networkPosition = new NetworkVariable<Vector3>();
+    public NetworkVariable<Vector3> networkRotation = new NetworkVariable<Vector3>();
+
+    Vector3 positionTarget;
+    Vector3 rotationTarget;
+    int tileSize = 5;
+    public float movementSpeed = 5.0f;
+    float movementGear;
+    float rotateGear;
+    bool startedMove = false;
+    public bool startedRotation = false;
+    float turnRate = 1.0f;
+
+    public override void OnNetworkSpawn()
+    {
+        SetGear(Gear.First);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (IsOwner)
+        {
+            if (Input.GetKeyDown("w"))
+                MoveForward();
+
+            //Rotation and movement can never occur at the same time
+            if (startedRotation)
+            {
+                //move if distant between rotation target and current rotation coordinates is larger than 0.006
+                if ((transform.forward - rotationTarget).magnitude > 0.006f)
+                {
+                    transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, rotationTarget, turnRate * rotateGear * Time.deltaTime, 0.0f));
+                }
+                //set rotation angle to the target angle and disable rotation
+                else
+                {
+                    transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.RoundToInt(transform.eulerAngles.y), transform.eulerAngles.z);
+                    startedRotation = false;
+                }
+            }
+
+
+            else if (startedMove)
+            {
+                //if the distance between the position target and the current position is larger than 0.000001, move towards it.
+                if ((transform.position - positionTarget).magnitude > 0.000001f)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, positionTarget, movementSpeed * movementGear * Time.deltaTime);
+                }
+                else
+                {
+                    //transform.position = positionTarget;
+                    startedMove = false;
+
+                }
+            }
+
+
+            UpdateNetworkInfoServerRpc(transform.position);
+        }
+
+        else
+        {
+            //Update model postition to match network position
+            transform.position = networkPosition.Value;
+
+        }
+
+    }
+
+    [ServerRpc]
+    public void UpdateNetworkInfoServerRpc(Vector3 newPos)
+    {
+        networkPosition.Value = newPos;
+
+    }
+
+
+    public bool IsDoingInstruction()
+    {
+        return (IsRotating() || IsMoving());
+    }
+
+    //Call on function to move robot forward in the direction it is facing
+    public void MoveForward()
+    {
+        if (IsDoingInstruction()) return;
+        positionTarget = transform.position + transform.forward * tileSize;
+        startedMove = true;
+    }
+    //Call on function to move robot backwards in the direction it is facing.
+    public void MoveBackwards()
+    {
+        if (IsDoingInstruction()) return;
+        positionTarget = transform.position - transform.forward * tileSize;
+        startedMove = true;
+    }
+    //Call on function to return whether robot is moving or not
+    public bool IsMoving()
+    {
+        return startedMove;
+    }
+
+    //Call on function to rotate the robot 90 degrees to the left
+    public void RotateLeft()
+    {
+        if (IsDoingInstruction()) return;
+
+        rotationTarget = -transform.right;
+        startedRotation = true;
+    }
+
+    //Call on function to rotate the robot 90 degrees to the right
+    public void RotateRight()
+    {
+        if (IsDoingInstruction()) return;
+        rotationTarget = transform.right;
+        startedRotation = true;
+    }
+
+    //Call on function to check whether robot is currently rotating
+    public bool IsRotating()
+    {
+        return startedRotation;
+    }
+
+    public void SetTileSize(int input)
+    {
+        tileSize = input;
+    }
+
+    //Call on function to set gear to either First, Second or Third.
+    public void SetGear(Gear newGear)
+    {
+        if (IsDoingInstruction()) return;
+
+        switch (newGear)
+        {
+            case Gear.First:
+                movementGear = 1;
+                rotateGear = 1;
+                break;
+            case Gear.Second:
+                movementGear = 2;
+                rotateGear = 1.5f;
+                break;
+            case Gear.Third:
+                movementGear = 3;
+                rotateGear = 2;
+                break;
+        }
+
+    }
+
+
+
+}
