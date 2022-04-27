7e6c499f8630fc2b278b68cc8930219f1cd0b958
Anton Rådberg <radberg.anton@gmail.com>
2022-04-27 15:58:44 +0200

    User interface to send instructions to robot

---
+++ Assets/Scripts/PushMoveForward.cs
@@ -0,0 +1,65 @@
+
+// using UnityEngine;
+// using UnityEngine.UI;
+// using TMPro;
+
+// public class PushMoveForward : MonoBehaviour
+// {
+
+//     public Canvas canvas;
+//     public Button btn;
+//     public TextMeshProUGUI tmp;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+        
+//         CreateButton(btn, canvas, tmp);
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         Instructions.
+//     }
+
+
+
+//     public static Button CreateButton(Button buttonPrefab, Canvas canvas, TextMeshProUGUI tempText)
+//     {   
+//         tempText.text = "69 420";
+//         Button tempButton = Object.Instantiate(buttonPrefab, new Vector3(286, 104, 0), Quaternion.identity);
+//         var rectTransform = tempButton.GetComponent<RectTransform>();
+//         rectTransform.SetParent(canvas.transform);
+//         //tempButton.GetComponentInChildren<TextMeshPro>().text = "Snorre";
+        
+
+//         //tempButton.GetComponentInChildren<Text>().text = "fel";
+
+//         return tempButton;
+//     }
+
+// }
+
+
+
+
+
+
+
+//         // Button btn = Object.Instantiate(buttonPrefab, Vector3.zero, Quaternion.identity);
+//         // Button mybtn = btn.GetComponent<Button>();
+//         // mybtn.GetComponentInChildren<Text>().text = "hejhej";
+        
+//         // var rectTransform = mybtn.GetComponent<RectTransform>();
+//         // rectTransform.SetParent(canvas.transform);
+//         // rectTransform.anchorMax = cornerTopRight;
+//         // rectTransform.anchorMin = cornerBottomLeft;
+//         // rectTransform.offsetMax = Vector2.zero;
+//         // rectTransform.offsetMin = Vector2.zero;
+//         // button.transform.position = new Vector3(286, 104, 0);
+//         // rectTransform.position = new Vector3(300, 130, 0);
+        
+//         // btn.GetComponent<Button>().GetComponentInChildren<Text>.text = "hejhej";
+//         // test.GetComponentInChildren<Text>().text = "bajs e najs";
+//         // return mybtn;
