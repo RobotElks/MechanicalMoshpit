b9fa62636eaa3d0753e19c081deb548e270baf90
Leo Åkerberg <leoakerberg@gmail.com>
2022-05-03 08:33:01 +0200

    Added if-else for detecting frontal collision

--- Assets/Scripts/Collision/RobotCollision.cs
+++ Assets/Scripts/Collision/RobotCollision.cs
@@ -19,14 +19,34 @@ public class RobotCollision : MonoBehaviour
         GameObject otherRobot = collision.gameObject;
         RobotMultiplayerMovement robotMovement = otherRobot.GetComponent<RobotMultiplayerMovement>();
 
+        Vector3 otherForward = otherRobot.transform.forward;
 
-
-        Debug.Log("Last position: " + robotMovement.GetLastPosition());
-        Debug.Log("Last rotation: " + robotMovement.GetLastRotation());
-        Debug.Log("Target position: " + robotMovement.GetTargetPosition());
-        Debug.Log("Target rotation: " + robotMovement.GetTargetRotation());
+        //Debug.Log("Last position: " + robotMovement.GetLastPosition());
+        //Debug.Log("Last rotation: " + robotMovement.GetLastRotation());
+        //Debug.Log("Target position: " + robotMovement.GetTargetPosition());
+        //Debug.Log("Target rotation: " + robotMovement.GetTargetRotation());
         this.GetComponent<Rigidbody>().isKinematic = true;
+        //Debug.Log(otherForward);
         Debug.Log("------------------------------------");
+
+        Vector3 coll = transform.forward + otherForward;
+        //Frontal Collision
+        if((transform.forward + otherForward).magnitude < 0.001f) {
+            //Check if other is moving or not 
+        }
+
+        else 
+        {
+            //List<ContactPoint> contactPoints = new List<ContactPoint>();
+            //collision.GetContacts(contactPoints);
+
+            //collision.collider.GetComponent<BoxCollider>().sc
+
+            ContactPoint contact = collision.contacts[0];
+
+            Debug.Log(transform.position);
+            Debug.Log(contact.thisCollider) ;
+        }
 
     }
 }
