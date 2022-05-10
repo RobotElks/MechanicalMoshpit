69c5af7628b1cc1a4e796813ec8bd660792a710e
Mille <millear@kth.se>
2022-05-10 11:39:35 +0200

    Display text and remove ui, hud etc, when losing and winning

--- Assets/Scripts/ChickenDinner.cs
+++ Assets/Scripts/ChickenDinner.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using TMPro;
 
 public class ChickenDinner : NetworkBehaviour
 {
     Dead deadScript;
     [SerializeField] RobotList robotList;
     [SerializeField] GameObject ui;
+    [SerializeField] GameObject hud;
+    [SerializeField] GameObject readyScreen;
+    [SerializeField] GameObject gameRoundsManager;
+    GameRoundsManager timer;
+    Transform winLose;
     GameObject[] robotArray;
 
     public void robotDeath() {
@@ -15,17 +21,39 @@ public class ChickenDinner : NetworkBehaviour
         for (int i = 0; i < robotArray.Length; i++) {
             deadScript = robotArray[i].GetComponent<Dead>();
             if(deadScript.IsDead()) {
-                Debug.Log("ROBot DEATH1");
                 robotList.RemoveRobot(robotArray[i]);
-                if (robotList.GetRobots().Length <= 1) {
+                if (robotList.GetRobots().Length <= 1 && !IsOwner) {
                     winner();
+                }
+                else {
+                    loser();
                 }
             }
         }   
     }
 
     public void winner() {
+        gameRoundsManager.SetActive(false);
+        readyScreen.SetActive(false);
         ui.SetActive(false);
+        hud.SetActive(false);
+        transform.Find("Canvas/Panel").gameObject.SetActive(true);
+        transform.Find("Canvas/Panel/LoserText").gameObject.SetActive(false);
+        winLose = transform.Find("Canvas/Panel/WinnerText");
+        winLose.gameObject.SetActive(true);
     }
-    
+
+    public void loser() {
+        if(IsOwner) {
+            gameRoundsManager.SetActive(false);
+            readyScreen.SetActive(false);
+            ui.SetActive(false);
+            hud.SetActive(false);
+            transform.Find("Canvas/Panel").gameObject.SetActive(true);
+            transform.Find("Canvas/Panel/WinnerText").gameObject.SetActive(false);
+            winLose = transform.Find("Canvas/Panel/LoserText");
+            winLose.GetComponent<TextMeshProUGUI>().text += "\n#" + (robotList.GetRobots().Length + 1);
+            winLose.gameObject.SetActive(true);
+        }
+    }
 }
