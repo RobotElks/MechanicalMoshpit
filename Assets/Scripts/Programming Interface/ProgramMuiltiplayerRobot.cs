da104bcd3ddfd4ef035d3891eb6739973d7a86b8
Robert <robert.minasyan1@gmail.com>
2022-05-19 13:02:34 +0200

    Added sounds to buttons and keyboard in-game

--- Assets/Scripts/Programming Interface/ProgramMuiltiplayerRobot.cs
+++ Assets/Scripts/Programming Interface/ProgramMuiltiplayerRobot.cs
@@ -34,6 +34,8 @@ public class ProgramMuiltiplayerRobot : MonoBehaviour
     public Button turnLeftButton;
     string turnLeftString = "Turn Left ";
 
+    public AudioSource clickAudio;
+
     public RobotMultiplayerInstructionScript instructionScript;
     GameObject robot;
     RobotEnergy energyScript;
@@ -91,22 +93,38 @@ public class ProgramMuiltiplayerRobot : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetKeyDown("w"))
+        if (Input.GetKeyDown("w")){
             addMoveForwardToProgram();
-        else if (Input.GetKeyDown("s"))
+            clickAudio.Play();
+        }
+        else if (Input.GetKeyDown("s")){
             addMoveBackwardToProgram();
-        else if (Input.GetKeyDown("a"))
+            clickAudio.Play();
+        }
+        else if (Input.GetKeyDown("a")){
             addTurnLeftToProgram();
-        else if (Input.GetKeyDown("d"))
+            clickAudio.Play();
+        }   
+        else if (Input.GetKeyDown("d")){
             addTurnRightToProgram();
-        else if (Input.GetKeyDown("q"))
+            clickAudio.Play();
+        }
+        else if (Input.GetKeyDown("q")){
             decreaseGear();
-        else if (Input.GetKeyDown("e"))
+            clickAudio.Play();
+        }
+        else if (Input.GetKeyDown("e")){
             increaseGear();
-        else if (Input.GetKeyDown(KeyCode.Backspace))
+            clickAudio.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.Backspace)){
             removeLastInstruction();
-        else if (Input.GetKeyDown(KeyCode.Return) && roundsScript.GetCurrentGameState() == GameState.Programming)
+            clickAudio.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && roundsScript.GetCurrentGameState() == GameState.Programming){
             FinishedProgramming();
+            clickAudio.Play();
+        }
     }
 
 
